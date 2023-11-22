613c65d1bc734d7000646e409a029218fec00c10
PawelOchal <opawel262@gmail.com>
2023-11-22 14:42:56 +0100

    fixed bug - wrong enriovment variable in settings v2

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('db_name'),
+        'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST'),
